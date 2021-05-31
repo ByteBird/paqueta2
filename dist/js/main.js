4a1d22311aa6c20f72e6f8b0a2a285f74be3944e
unknown <jhonatan_dmz@hotmail.com>
2021-05-31 05:05:45 -0500

    gaaa

---
+++ dist/js/main.js
@@ -0,0 +1,10 @@
+const config = {
+    type: 'carousel',
+    perView: 2,
+    gap:20,
+    autoplay: 7000,
+    focusAt: 'center',
+    animationDuration: 1500
+}
+
+new Glide('.glide',config).mount()
